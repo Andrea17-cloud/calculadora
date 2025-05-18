e9e4b170d3a8d5533d75d326501ba6eb1563d232
Andrea17-cloud <andreajimenajeronimoa@gmail.com>
2025-05-17 23:10:23 -0600

    commit

--- src/pages/calculadora.jsx
+++ src/pages/calculadora.jsx
@@ -49,27 +49,35 @@ function CalculadoraGrafica() {
         <div className="calculadora">
             <div className="display">{display}</div>
             <div className="teclado">
+                <button className="limpiar" onClick={() => manejarClicBoton('C')}>AC</button>
+                <button className="limpiar" ></button>
+                <button className="limpiar" ></button>
+                <button className="operador" onClick={() => manejarClicBoton('/')}>/</button>
+
                 <button onClick={() => manejarClicBoton('7')}>7</button>
                 <button onClick={() => manejarClicBoton('8')}>8</button>
                 <button onClick={() => manejarClicBoton('9')}>9</button>
-                <button className="operador" onClick={() => manejarClicBoton('/')}>/</button>
+                <button className="operador" onClick={() => manejarClicBoton('*')}>x</button>
+                
 
                 <button onClick={() => manejarClicBoton('4')}>4</button>
                 <button onClick={() => manejarClicBoton('5')}>5</button>
                 <button onClick={() => manejarClicBoton('6')}>6</button>
-                <button className="operador" onClick={() => manejarClicBoton('*')}>*</button>
+                <button className="operador" onClick={() => manejarClicBoton('-')}>-</button>
+                
 
                 <button onClick={() => manejarClicBoton('1')}>1</button>
                 <button onClick={() => manejarClicBoton('2')}>2</button>
                 <button onClick={() => manejarClicBoton('3')}>3</button>
-                <button className="operador" onClick={() => manejarClicBoton('-')}>-</button>
-
+                <button className="operador" onClick={() => manejarClicBoton('+')}>+</button>
+                
+                <button ></button>
                 <button onClick={() => manejarClicBoton('0')}>0</button>
                 <button onClick={() => manejarClicBoton('.')}>.</button>
                 <button className="igual" onClick={() => manejarClicBoton('=')}>=</button>
-                <button className="operador" onClick={() => manejarClicBoton('+')}>+</button>
+                
 
-                <button className="limpiar" onClick={() => manejarClicBoton('C')}>C</button>
+                
             </div>
         </div>
     );
